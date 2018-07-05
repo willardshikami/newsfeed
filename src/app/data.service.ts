c7da97afb19d8d5e5c4d76471798690174dbcc4e
willardshikami <willardshikami@gmail.com>
2018-07-05 19:33:13 +0300

    Loaded data to more components

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -16,12 +16,52 @@ export class DataService {
   //URL to world top stories
   private topStories: string = "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=0ab09f545d204281a59195d5f70510fe";
 
+  //URL to tech stories
+  private techStories: string = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0ab09f545d204281a59195d5f70510fe"
+
+  //URL to sports News
+  private sportsNews: string = "https://newsapi.org/v2/top-headlines?sources=bleacher-report&apiKey=0ab09f545d204281a59195d5f70510fe"
+
+  //worldNews
+  private worldNews = "https://newsapi.org/v2/top-headlines?sources=bloomberg&apiKey=0ab09f545d204281a59195d5f70510fe"
+
+  //Business News
+  private businessNews: string = "https://newsapi.org/v2/top-headlines?sources=business-insider-uk&apiKey=0ab09f545d204281a59195d5f70510fe"
+
+  //science news
+  private scienceNews: string = "https://newsapi.org/v2/top-headlines?sources=wired&apiKey=0ab09f545d204281a59195d5f70510fe"
+
   //inject HttpClient
   constructor( private http: HttpClient) {}
 
   //get topstories response
   getTopStories(){
-    return this.http.get<stories>(this.topStories)
+    return this.http.get<stories>(this.topStories);
+  }
+
+  //Tech stories
+  getTechStories(){
+    return this.http.get<any>(this.techStories);
+  }
+
+  //sports news
+  getSportsNews(){
+    return this.http.get<any>(this.sportsNews);
+  }
+
+  //get worldnews
+  getWorldNews(){
+    return this.http.get<any>(this.worldNews);
+  }
+
+  //business news
+  getBusinessNews(){
+    return this.http.get<any>(this.businessNews);
+  }
+
+  //get science news
+  getScienceNews(){
+    return this.http.get<any>(this.scienceNews);
   }
 }
  
--- src/app/sports/sports.component.ts
+++ src/app/sports/sports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
 
 @Component({
   selector: 'sports',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SportsComponent implements OnInit {
 
-  constructor() { }
+  sportsNews: any
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getSportsNews()
+    .subscribe(
+      data => this.sportsNews = data.articles
+    )
   }
 
 }
