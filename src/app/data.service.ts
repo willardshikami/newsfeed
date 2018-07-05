45713ea0c22e03118beff87ac12a2e1a3b22e25c
willardshikami <willardshikami@gmail.com>
2018-07-05 13:26:09 +0300

    loading news res from api to component

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
-
+//stories interface
 interface stories{
   articles: Object
 }
@@ -13,12 +13,13 @@ interface stories{
 
 export class DataService {
 
+  //URL to world top stories
   private topStories: string = "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=0ab09f545d204281a59195d5f70510fe";
 
-  constructor( 
-    private http: HttpClient) { }
+  //inject HttpClient
+  constructor( private http: HttpClient) {}
 
-  //Top Stories
+  //get topstories response
   getTopStories(){
     return this.http.get<stories>(this.topStories)
   }
--- src/app/top-stories/top-stories.component.ts
+++ src/app/top-stories/top-stories.component.ts
@@ -9,16 +9,17 @@ import { DataService } from '../data.service';
 })
 export class TopStoriesComponent implements OnInit {
 
-  _stories: any;
+  //declare an empty stories var
+  private _stories: any;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    //loading the response from the service and subscribing to it to display the data on the components
     this.dataService.getTopStories()
     .subscribe(
-      data => {
-        this._stories = data.articles;
-      }
+      data => this._stories = data.articles,
+      error => console.log(error)
     )
   }
 
