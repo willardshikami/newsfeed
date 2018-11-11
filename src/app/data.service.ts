91a2a4d28ab4d07c966868289c6d64e07a35582c
willardshikami <willardshikami@gmail.com>
2018-11-11 14:37:17 +0300

    refactoring

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,83 +1,85 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-//stories interface
-interface stories {
-  articles: Object;
+// stories interface
+interface Stories {
+  articles: {};
 }
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
+
 export class DataService {
-  //URL to world top stories
-  private topStoriesUrl: string =
-    "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=0ab09f545d204281a59195d5f70510fe";
+  // URL to world top stories
+  private topStoriesUrl =
+    'https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=0ab09f545d204281a59195d5f70510fe';
 
-  //URL to tech stories
-  private techStoriesUrl: string =
-    "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0ab09f545d204281a59195d5f70510fe";
+  // URL to tech stories
+  private techStoriesUrl =
+    'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0ab09f545d204281a59195d5f70510fe';
 
-  //URL to sports News
-  private sportsNewsUrl: string =
-    "https://newsapi.org/v2/top-headlines?sources=bleacher-report&apiKey=0ab09f545d204281a59195d5f70510fe";
+  // URL to sports News
+  private sportsNewsUrl =
+    'https://newsapi.org/v2/top-headlines?sources=bleacher-report&apiKey=0ab09f545d204281a59195d5f70510fe';
 
-  //worldNews
+  // worldNews
   private worldNewsUrl =
-    "https://newsapi.org/v2/top-headlines?sources=bloomberg&apiKey=0ab09f545d204281a59195d5f70510fe";
+    'https://newsapi.org/v2/top-headlines?sources=bloomberg&apiKey=0ab09f545d204281a59195d5f70510fe';
 
-  //Business News
-  private businessNewsUrl : string =
-    "https://newsapi.org/v2/top-headlines?sources=business-insider-uk&apiKey=0ab09f545d204281a59195d5f70510fe";
+  // Business News
+  private businessNewsUrl =
+    'https://newsapi.org/v2/top-headlines?sources=business-insider-uk&apiKey=0ab09f545d204281a59195d5f70510fe';
 
-  //science news
-  private scienceNewsUrl: string =
-    "https://newsapi.org/v2/top-headlines?sources=new-scientist&apiKey=0ab09f545d204281a59195d5f70510fe";
+  // science news
+  private scienceNewsUrl =
+    'https://newsapi.org/v2/top-headlines?sources=new-scientist&apiKey=0ab09f545d204281a59195d5f70510fe';
 
-  private entertaimentUrl: string = "https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=0ab09f545d204281a59195d5f70510fe"
+  private entertaimentUrl =
+  'https://newsapi.org/v2/top-headlines?sources=entertainment-weekly&apiKey=0ab09f545d204281a59195d5f70510fe';
 
-  private medicalUrl: string = "https://newsapi.org/v2/top-headlines?sources=medical-news-today&apiKey=0ab09f545d204281a59195d5f70510fe"
+  private medicalUrl = 'https://newsapi.org/v2/top-headlines?sources=medical-news-today&apiKey=0ab09f545d204281a59195d5f70510fe';
 
-  //inject HttpClient
-  constructor(private http: HttpClient) {}
+  // inject HttpClient
+  constructor(private http: HttpClient) { }
 
-  //get topstories response
+  // get topstories response
   getTopStories() {
-    return this.http.get<stories>(this.topStoriesUrl)
+    return this.http.get<Stories>(this.topStoriesUrl);
   }
 
-  //Tech stories
+  // Tech stories
   getTechStories() {
-    return this.http.get<any>(this.techStoriesUrl)
+    return this.http.get<Stories>(this.techStoriesUrl);
   }
 
-  //sports news
+  // sports news
   getSportsNews() {
-    return this.http.get<any>(this.sportsNewsUrl)
+    return this.http.get<Stories>(this.sportsNewsUrl);
   }
 
-  //get worldnews
+  // get worldnews
   getWorldNews() {
-    return this.http.get<any>(this.worldNewsUrl)
+    return this.http.get<Stories>(this.worldNewsUrl);
   }
 
-  //business news
+  // business news
   getBusinessNews() {
-    return this.http.get<any>(this.businessNewsUrl)
+    return this.http.get<Stories>(this.businessNewsUrl);
   }
 
-  //get science news
+  // get science news
   getScienceNews() {
-    return this.http.get<any>(this.scienceNewsUrl)
+    return this.http.get<Stories>(this.scienceNewsUrl);
   }
 
-  //get Entertainment News
-  getEntertainmentNews(){
-    return this.http.get<any>(this.entertaimentUrl)
+  // get Entertainment News
+  getEntertainmentNews() {
+    return this.http.get<Stories>(this.entertaimentUrl);
   }
 
-  //medical news 
-  getMedicalNews(){
-    return this.http.get<any>(this.medicalUrl)
+  // medical news
+  getMedicalNews() {
+    return this.http.get<Stories>(this.medicalUrl);
   }
 }
--- src/app/world-news/world-news.component.ts
+++ src/app/world-news/world-news.component.ts
@@ -2,22 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 
 @Component({
-  selector: 'world-news',
+  selector: 'app-world-news',
   templateUrl: './world-news.component.html',
   styleUrls: ['./world-news.component.css']
 })
 export class WorldNewsComponent implements OnInit {
 
-  worldnews: any
+  worldnews: any;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.dataService.getWorldNews()
     .subscribe(
-      data => this.worldnews = data.articles,
-      error => console.log(error)
-    )
+      data => this.worldnews = data.articles
+    );
   }
-
 }
